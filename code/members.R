3c1edc4cb16f2ba679c4b44e2aba159f81d8e8e8
alice-hannah <alice.hannah@gov.scot>
2024-11-15 16:17:16 +0000

    Include outside collaborators and produce summary

--- code/members.R
+++ code/members.R
@@ -1,28 +1,61 @@
-
-# Requires GitHub PAT 
-# Set up using: usethis::create_github_token()
+# Analyse organisation members and outside collaborators
+# Requires GitHub PAT. Set up using: usethis::create_github_token()
 
 library(gh)
-library(purrr)
 library(dplyr)
+library(tidyr)
 
-# Get organisation members
+# 1 - Get tibble of members and outside collaborators ----
 
 members <- 
-  gh("GET /orgs/{org}/members",
-     org = "ScotGovAnalysis",
-     per_page = 100) |>
-  map_chr(\(x) x$login)
+  tibble(type = "org_member",
+         member = gh("GET /orgs/{org}/members",
+                     org = "ScotGovAnalysis",
+                     per_page = 100)) %>%
+  hoist(member, username = "login")
 
-# Get public emails for members
+outside_colabs <- 
+  tibble(type = "outside_colab",
+         member = gh("GET /orgs/{org}/outside_collaborators",
+                     org = "ScotGovAnalysis",
+                     per_page = 100)) %>%
+  hoist(member, username = "login")
 
-email_fn <- function(user) {
-  gh("GET /users/{username}", username = user)$email
-}
+all_users <- 
+  bind_rows(members, outside_colabs) %>%
+  rowwise() %>%
+  mutate(public_info = list(gh("GET /users/{username}", 
+                               username = username))) %>%
+  ungroup() %>%
+  hoist(public_info,
+        name = "name",
+        company = "company",
+        email = "email")
 
-email <-
-  members |>
-  map_dfr(
-    \(x) tibble(user = x, email = email_fn(x))
-  )
-  
+
+# 2 - Summary of member type and whether email public ----
+
+summary <- 
+  all_users %>%
+  group_by(type) %>%
+  summarise(total = n(),
+            email_available = sum(!is.na(email)),
+            email_missing = sum(is.na(email)),
+            .groups = "drop") 
+
+summary
+
+
+# 3 - Tibble of members to follow up ----
+
+# Members of interest:
+#  - SG staff who are outside collaborators
+#  - Members with no public email address
+
+follow_up <- 
+  all_users %>%
+  filter(type == "outside_colab" | is.na(email)) %>%
+  select(type, username, email, name, company)
+
+
+### END OF SCRIPT ###
